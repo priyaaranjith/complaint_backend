98ba5a838e452c62278f169884cb79ec0bca042b
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-09 14:23:25 +0530

    first

---
+++ src/main/java/com/example/complaint_backend/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.complaint_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+    @GetMapping("/")
+    public String HomePage(){
+        return "Welcome";
+    }
+}
