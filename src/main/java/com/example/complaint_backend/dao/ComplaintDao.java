5c889fe0cfebb66428f2ed479af82bca8e80e834
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-10 11:01:00 +0530

    log

--- src/main/java/com/example/complaint_backend/dao/ComplaintDao.java
+++ src/main/java/com/example/complaint_backend/dao/ComplaintDao.java
@@ -1,9 +1,18 @@
 package com.example.complaint_backend.dao;
 
 import com.example.complaint_backend.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ComplaintDao extends CrudRepository<User,Integer> {
+    @Query(value = "SELECT `id`, `address`, `email`, `mobile`, `name`, `password`, `username` FROM `user_reg` WHERE `username` = :username AND `password` = :password",nativeQuery = true)
+    List<User> UserLogin(@Param("username") String username , @Param("password") String password);
+
+    @Query(value = "SELECT `id`, `address`, `email`, `mobile`, `name`, `password`, `username` FROM `user_reg` WHERE `username`= :username",nativeQuery = true)
+    List<User> FindUser(@Param("username") String username);
 
 
 }
